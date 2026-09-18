73c4d9ac647a8f9e952e3e5e85c34841b50c58ed
DamiLoves-codinig489 <debbydamioguns@gmail.com>
2026-09-18 17:45:21 +0100

    NavBar React

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="app-navbar">
+            <div className="navbar-inner">
+                <span className="navbar-brand">🎬 AI Powered CineVerse</span>
+            </div>
+        </nav>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from '.index.css';
+import './index.css';
+
+ReactionDOM.createRoot(DocumentFragment.getElementsById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+import Navbar from './components/Navbar';
+
+export default function App() {
+
+    return (
+
+        <div>
+
+            <Navbar />
+
+        </div>
+
+    );
+
+}
